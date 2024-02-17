f0e3ce653196c72f480a2de1be69756e8d5881d7
Oleksii Neshcheret <nesheret.a.s@outlook.com>
2024-02-17 19:43:21 +0100

    disable duplicates during import

--- Services/CallService.cs
+++ Services/CallService.cs
@@ -21,10 +21,19 @@ namespace CDR.Services
                 var reader = new StreamReader(file);
                 var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ",", HasHeaderRecord = true };
                 using var csv = new CsvReader(reader, csvConfig);
-                var records = csv.GetRecords<CallDetail>().ToArray();
-                _dbContext.CallDetails.AddRange(records);
+                var records = csv.GetRecords<CallDetail>().ToList();
+                var count = 0;
+                foreach (var record in records)
+                {
+                    if (_dbContext.CallDetails.FirstOrDefault(r => r.CallerId == record.CallerId && r.Recipient == record.Recipient 
+                                                        && r.CallDate == record.CallDate && r.EndTime == record.EndTime) != null)
+                    {
+                        _dbContext.CallDetails.Add(record);
+                        count++;
+                    }
+                }
                 await _dbContext.SaveChangesAsync();
-                return records.Count();
+                return count;
             }
             catch (Exception)
             {
